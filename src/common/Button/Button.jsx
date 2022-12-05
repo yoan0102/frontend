e3b8abeb4df67b6ebc3af63558c1d4980571575b
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-12-05 16:27:31 -0500

    feature : adding button common

---
+++ src/common/Button/Button.jsx
@@ -0,0 +1,13 @@
+import './styles/Button.scss';
+
+const Button = () => {
+  return (
+    <div className="cont-button">
+      <button className="btn-bubble">
+        <span>Bubble</span>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
