7ce275623c43498b64b3d43eb52dce0c11eff770
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-12-09 10:11:42 -0500

    fix: refactiring in implemnts react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider, QueryClient } from 'react-query'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
 
 import App from './App'
 
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './styles/main.scss'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
