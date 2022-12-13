ef3178eefa16a797ccf6224f089ab11173072201
Yanet Brito <yabf0801@gmail.com>
2022-12-13 17:47:12 -0500

    fix users screen

--- src/views/dashboard/Users/Users.jsx
+++ src/views/dashboard/Users/Users.jsx
@@ -1,14 +1,13 @@
 import UserList from './components/UsersList/UserList';
 import { UserProvider } from './context/UserContext';
 
-import UserForm from './components/forms/UserForm';
 import './styles/Users.scss';
 
 const Users = () => {
 	return <div className='users'>
 		<UserProvider>
 			<UserList />
- 		</UserProvider>
+		</UserProvider>
 	</div >;
 };
 
