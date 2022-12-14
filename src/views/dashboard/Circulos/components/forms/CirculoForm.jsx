a5ec4e1bc886d6052c1668733c2a101dc2598af8
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-12-14 15:48:03 -0500

    fix: adding message errors circulo form

--- src/views/dashboard/Circulos/components/forms/CirculoForm.jsx
+++ src/views/dashboard/Circulos/components/forms/CirculoForm.jsx
@@ -13,51 +13,13 @@ const REQUIRED = 'Este campo es requerido';
 
 
 const CirculoSchema = Yup.object().shape({
-	noEntry: Yup.number().required(REQUIRED),
-	type: Yup.string().required(REQUIRED),
-	social_case: Yup.boolean(),
-	motive: Yup.string(),
-	children: Yup.object().shape({
-		nameChildren: Yup.string().required(REQUIRED),
-		lastNameChildren: Yup.string().required(REQUIRED),
-		lastNameChildren2: Yup.string(),
-		noIdentity: Yup.number()
-			.test('len', 'EL carnet de identidad necesita 11 digitos', (val) => val?.toString().length === 11)
-			.required(REQUIRED),
-		age: Yup.number(),
-		year_of_life: Yup.number().required(REQUIRED),
-		sex: Yup.string(),
-		street: Yup.string().required(REQUIRED),
-		between: Yup.string(),
-		house: Yup.string(),
-		stair: Yup.string(),
-		locality: Yup.string().required(REQUIRED),
-		cPopular: Yup.string().required(REQUIRED),
-		municipality: Yup.string().required(REQUIRED),
-		province: Yup.string(),
-		parents: Yup.object().shape({
-			fullNameParent: Yup.string().required(REQUIRED),
-			uniqueParent: Yup.boolean(),
-			addressParent: Yup.string(),
-			phoneNumberParent: Yup.string().required(REQUIRED),
-			typeParent: Yup.string().required(REQUIRED),
-			occupation: Yup.array().required(REQUIRED),
-			convivencia: Yup.boolean(),
-			work: Yup.string().required(REQUIRED),
-			workAddress: Yup.string().required(REQUIRED),
-			workPosition: Yup.string(),
-			organismo: Yup.string().required(REQUIRED),
-			workPhoneParent: Yup.string(),
-			horario: Yup.string(),
-			salary: Yup.number(),
-			otherChildren: Yup.boolean(),
-			cantOtherChildren: Yup.number(),
-			ciOtherChildren: Yup.string(),
-			pregnant: Yup.boolean(),
-			student: Yup.boolean(),
-			deaf: Yup.boolean(),
-		}),
-	}),
+	no: Yup.number().required(REQUIRED),
+	name: Yup.string().required(REQUIRED),
+	normed_capacity2: Yup.number().required(REQUIRED),
+	normed_capacity3: Yup.number().required(REQUIRED),
+	normed_capacity4: Yup.number().required(REQUIRED),
+	normed_capacity5: Yup.number().required(REQUIRED),
+	normed_capacity6: Yup.number().required(REQUIRED),
 });
 
 function CirculoForm() {
@@ -92,7 +54,7 @@ function CirculoForm() {
 		>
 			<div className='card'>
 				<div className='card-header'>
-					<h3>Nuevo Circulo</h3>
+					<h2>Nuevo Circulo</h2>
 				</div>
 				<div className='card-body'>
 					<form onSubmit={form.handleSubmit}>
@@ -110,6 +72,7 @@ function CirculoForm() {
 										onChange={form.handleChange}
 										value={form.values.no}
 									/>
+									{form.errors.no ? <p>{form.errors.no}</p> : null}
 								</div>
 
 								<div className='col-md-10 mb-3'>
@@ -124,6 +87,8 @@ function CirculoForm() {
 										onChange={form.handleChange}
 										value={form.values.name}
 									/>
+									{form.errors.name ? <p>{form.errors.name}</p> : null}
+
 								</div>
 							</div>
 						</div>
@@ -142,62 +107,78 @@ function CirculoForm() {
 								</div>
 
 								<div className='col-md-2 mb-3 justify-content-between'>
-									<label>Capacidad por año</label>
+									<h3>Capacidad por año</h3>
 
-									<label>2do año</label>
-									<input
-										type='number'
-										className='form-control'
-										name='normed_capacity2'
-										id='normed_capacity2'
-										required
-										onChange={form.handleChange}
-										value={form.values.normed_capacity2}
-									/>
+									<div className='mb-2'>
+										<label>2do año</label>
+										<input
+											type='number'
+											className='form-control'
+											name='normed_capacity2'
+											id='normed_capacity2'
+											required
+											onChange={form.handleChange}
+											value={form.values.normed_capacity2}
+										/>
+										{form.errors.normed_capacity2 ? <p>{form.errors.normed_capacity2}</p> : null}
+									</div>
 
-									<label>3er año</label>
-									<input
-										type='number'
-										className='form-control'
-										name='normed_capacity3'
-										id='normed_capacity3'
-										required
-										onChange={form.handleChange}
-										value={form.values.normed_capacity3}
-									/>
+									<div className='mb-2'>
+										<label>3er año</label>
+										<input
+											type='number'
+											className='form-control'
+											name='normed_capacity3'
+											id='normed_capacity3'
+											required
+											onChange={form.handleChange}
+											value={form.values.normed_capacity3}
+										/>
+										{form.errors.normed_capacity3 ? <p>{form.errors.normed_capacity3}</p> : null}
 
-									<label>4to año</label>
-									<input
-										type='number'
-										className='form-control'
-										name='normed_capacity4'
-										id='normed_capacity4'
-										required
-										onChange={form.handleChange}
-										value={form.values.normed_capacity4}
-									/>
+									</div>
 
-									<label>5to año</label>
-									<input
-										type='number'
-										className='form-control'
-										name='normed_capacity5'
-										id='normed_capacity5'
-										required
-										onChange={form.handleChange}
-										value={form.values.normed_capacity5}
-									/>
+									<div className='mb-2'>
+										<label>4to año</label>
+										<input
+											type='number'
+											className='form-control'
+											name='normed_capacity4'
+											id='normed_capacity4'
+											required
+											onChange={form.handleChange}
+											value={form.values.normed_capacity4}
+										/>
+										{form.errors.normed_capacity4 ? <p>{form.errors.normed_capacity4}</p> : null}
+									</div>
 
-									<label>6to año</label>
-									<input
-										type='number'
-										className='form-control'
-										name='normed_capacity6'
-										id='normed_capacity6'
-										required
-										onChange={form.handleChange}
-										value={form.values.normed_capacity6}
-									/>
+									<div className='mb-2'>
+										<label>5to año</label>
+										<input
+											type='number'
+											className='form-control'
+											name='normed_capacity5'
+											id='normed_capacity5'
+											required
+											onChange={form.handleChange}
+											value={form.values.normed_capacity5}
+										/>
+										{form.errors.normed_capacity5 ? <p>{form.errors.normed_capacity5}</p> : null}
+									</div>
+
+									<div className='mb-2'>
+										<label>6to año</label>
+										<input
+											type='number'
+											className='form-control'
+											name='normed_capacity6'
+											id='normed_capacity6'
+											required
+											onChange={form.handleChange}
+											value={form.values.normed_capacity6}
+										/>
+										{form.errors.normed_capacity6 ? <p>{form.errors.normed_capacity6}</p> : null}
+									</div>
 
 								</div>
 							</div>
